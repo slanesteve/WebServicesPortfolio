572c599f38bed708f7acf11f38870bf8027f3faa
slanesteve <steve.l.lane@gmail.com>
2025-01-13 18:41:43 -0500

    simple demo with js and css added

---
+++ script/effects.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('nav ul li a');
+
+    for (const link of links) {
+        link.addEventListener('click', smoothScroll);
+    }
+
+    function smoothScroll(event) {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+});
