258f1059c2b4a196654ac2ab60f89c2a903a08a9
slanesteve <steve.l.lane@gmail.com>
2025-01-13 19:24:20 -0500

    added some working js code now

--- script/effects.js
+++ script/effects.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('nav ul li a');
+    const links = document.querySelectorAll('a');
 
-    for (const link of links) {
-        link.addEventListener('click', smoothScroll);
-    }
-
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = event.currentTarget.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
+    links.forEach(link => {
+        link.addEventListener('mouseover', function () {
+            link.style.color = 'red';
+            link.style.fontWeight = 'bold';
         });
-    }
+
+        link.addEventListener('mouseout', function () {
+            link.style.color = 'white';
+            link.style.fontWeight = 'normal';   
+        });
+    });
 });
